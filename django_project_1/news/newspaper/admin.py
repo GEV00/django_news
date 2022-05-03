0f0175090a725b7f4e66708afadaf2a5cdbd5111
GEV00 <103899373+GEV00@users.noreply.github.com>
2022-05-03 18:28:00 +0300

    Delete django_project_1/news directory

--- django_project_1/news/newspaper/admin.py
+++
@@ -1,7 +0,0 @@
-from atexit import register
-from django.contrib import admin
-from newspaper.models import News, Comments
-# Register your models here.
-@admin.register(News, Comments)
-class ModelAdmin(admin.ModelAdmin):
-    pass
