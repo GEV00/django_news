1205b587ee5f53cd646b6bb464fdc83dd50560d5
Ernest <grigoren99@mail.ru>
2022-05-04 20:24:24 +0300

    commit | task 6.6 complition

--- news/newspaper/admin.py
+++ news/newspaper/admin.py
@@ -10,13 +10,37 @@ class CommentsInLine(admin.TabularInline):
     model = Comments
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content']
+    list_display = ['title', 'created_at', 'updated_at', 'active']
+    list_filter = ['active']
     #Подвязываем inlines через одноименное поле
     inlines = [CommentsInLine]
+    #Описываем групповые дейстия:
+    #список действий
+    actions = ['activate', 'deactivate']
+    #описание первого действия
+    def activate(self, request, queryset):
+        queryset.update(active=True)
+    #описание второго дейтсвия
+    def deactivate(self, request, queryset):
+        queryset.update(active=False)
+    #визуализация действий в админке в выпадающем списке
+    activate.short_description = 'Перевести новость в Активные'
+    deactivate.short_description = 'Убрать новость из Активных'
 
 class CommentsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'content', 'username']
+    list_display = ['username', 'content', 'new', 'delete_status']
     list_filter = ['username']
+
+    actions = ['deleted', 'non_deleted']
+
+    def deleted(self, request, queryset):
+        queryset.update(delete_status=True)
+    
+    def non_deleted(self, request, queryset):
+        queryset.update(delete_status=False)
+
+    deleted.short_description = 'Пометить как "Удаленный администратором"'
+    non_deleted.short_description = 'Убрать отметку об удалении'
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Comments, CommentsAdmin)
--- news/newspaper/models.py
+++ news/newspaper/models.py
@@ -1,17 +1,25 @@
+from secrets import choice
 from django.db import models
 from matplotlib.pyplot import cla
 
 # Create your models here.
 class Comments(models.Model):
 
+    DELETE_CHOICES = [
+        (True, 'Удалено администратором'),
+        (False, '')
+    ]
+
     username = models.CharField(max_length=30, verbose_name='Имя')
     content = models.TextField(verbose_name='Текст')
+    delete_status = models.BooleanField(default=False, choices=DELETE_CHOICES,
+                                         verbose_name='Статус')
 
     new = models.ForeignKey('News', on_delete=models.CASCADE, null=True,
                             related_name='news', verbose_name='Новость')
 
     def __str__(self):
-        return self.username
+        return f"{self.username}, {self.content[:15]}..."
     
     class Meta:
         db_table = 'Комментарии'
@@ -19,14 +27,22 @@ class Comments(models.Model):
 
 class News(models.Model):
 
+    #Список тьюплов для параметра choices для групповых действий
+    ACTIVITY_CHOICES = [
+        (True, 'Active'),
+        (False, 'Deactive')
+    ]
+
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Содержание')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    #параметр choices задаеся для групповых действий
+    active = models.BooleanField(default=True, choices=ACTIVITY_CHOICES)
 
     def __str__(self):
-        return self.title
+        #через split убираем отображение долей секунды
+        return f"{self.title} | {str(self.created_at).split('.')[0]} | Активность: {self.active}"
 
     class Meta:
         db_table = 'Список новостей'
