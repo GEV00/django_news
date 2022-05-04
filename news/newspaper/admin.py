578f8244dde21792f1c3635fa3268214dbd5ccab
Ernest <rest.grigoryan@mail.ru>
2022-05-04 11:25:14 +0300

    commit | task 6.3

--- news/newspaper/admin.py
+++ news/newspaper/admin.py
@@ -2,6 +2,21 @@ from atexit import register
 from django.contrib import admin
 from newspaper.models import News, Comments
 # Register your models here.
-@admin.register(News, Comments)
-class ModelAdmin(admin.ModelAdmin):
-    pass
+
+#Применяем класс Tabular(Stacked)Inline к модели, в которой описана
+#связь ForeignKey. В нашем случае в Commets есть поле new - связь
+#ForeignKey с News.
+class CommentsInLine(admin.TabularInline):
+    model = Comments
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+    #Подвязываем inlines через одноименное поле
+    inlines = [CommentsInLine]
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'username']
+    list_filter = ['username']
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Comments, CommentsAdmin)
